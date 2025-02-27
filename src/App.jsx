c4382a9ea25ce0c8cfd1fa00c68262ca9eb5ea24
Ylenia <yleniamighela@gmail.com>
2025-02-27 15:44:52 +0100

    added api call ( to see data in console) and includes global content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,56 @@
+// importo i componenti
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+
+// importo il global context
+import GlobalContexts from "./contexts/GlobalContexts";
+
+// importo la chiamata axios
+import axios from "axios";
+
+
+// importo lo useState e UseEffect
+import { useState, useEffect } from 'react';
 
 
 function App() {
 
+  const [film, setFilm] = useState([]);
+
+
+  //funzione di gestione chiamate API
+  function fetchFilm() {
+    axios.get("https://api.themoviedb.org/3/search/movie?api_key=78b08f89c5f899c994084e5ca136fe68&query=disney")
+      .then((res) => {
+        setFilm(res.data.results);
+        // console.log(res.data.results);
+
+      })
+
+      .catch(err => console.log(err));
+  }
+
+  useEffect(fetchFilm, []);
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <>
+      <GlobalContexts.Provider value={{ film }}>
 
-      <Header />
-      <Main />
+        <Header />
+        <Main />
+
+      </GlobalContexts.Provider>
     </>
   )
 }
