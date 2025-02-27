f6f7916e2a86e7a54cc6e7f17ce790507fbfcb7f
Ylenia <yleniamighela@gmail.com>
2025-02-27 21:57:07 +0100

    added text in FilmCard

--- src/components/main/FilmCard.jsx
+++ src/components/main/FilmCard.jsx
@@ -13,10 +13,11 @@ export default function FilmCard(props) {
 
             <div className="postitem" key={film.id}>
 
-                <h2>{film.title}</h2>
-                <h3>{film.original_title} </h3>
-                <span>{film.original_language}</span>
-                <span>{film.vote_average}</span>
+                <h2>Titolo {film.title}</h2>
+                <h3> Titolo originale {film.original_title} </h3>
+                <span> Lingua {film.original_language}</span>
+                <br />
+                <span> Voto {film.vote_average}</span>
 
             </div>
 
