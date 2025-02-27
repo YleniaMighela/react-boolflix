121bcff11e91e7e03546dac4f66dadd381415f11
Ylenia <yleniamighela@gmail.com>
2025-02-27 14:05:36 +0100

    added contexts

---
+++ src/contexts/GlobalContexts.jsx
@@ -0,0 +1,10 @@
+// importo il global contexts che darà modo di dare i dati ai componenti in cui verrà importato
+
+
+import { createContext } from "react";
+
+
+const GlobalContexts = createContext();
+
+
+export default GlobalContexts;
