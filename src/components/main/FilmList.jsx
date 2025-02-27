5ef61a8252e1eb5c336b0cbaad9a4f5c0cb14a4a
Ylenia <yleniamighela@gmail.com>
2025-02-27 17:33:50 +0100

    added ccs basic

--- src/components/main/FilmList.jsx
+++ src/components/main/FilmList.jsx
@@ -10,15 +10,16 @@ export default function FilmList() {
 
 
     return (
-        <div>
+        <div className="container" >
             {films.map((film) => (
+                <div className="postitem">
+                    <div key={film.id}>
+                        <h2>{film.title}</h2>
+                        <h3>{film.original_title} </h3>
+                        <span>{film.original_language}</span>
+                        <span>{film.vote_average}</span>
 
-
-                <div>
-                    <h2>{film.title}</h2>
-                    <h3>{film.original_title} </h3>
-                    <span>{film.original_language}</span>
-                    <span>{film.vote_average}</span>
+                    </div>
                 </div>
 
 
