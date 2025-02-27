384c8ce939a5144e57c775dd2c3b29950e8b98d5
Ylenia <yleniamighela@gmail.com>
2025-02-27 17:47:04 +0100

    split the files

--- src/components/main/FilmCard.jsx
+++ src/components/main/FilmCard.jsx
@@ -1,1 +1,29 @@
 // qui genero a lista delle card
+
+export default function FilmCard(props) {
+
+    const { film } = props;
+
+
+
+
+    return (
+
+        <div className="container" >
+
+            <div className="postitem" key={film.id}>
+
+                <h2>{film.title}</h2>
+                <h3>{film.original_title} </h3>
+                <span>{film.original_language}</span>
+                <span>{film.vote_average}</span>
+
+            </div>
+
+
+
+        </div>
+    )
+
+
+}
--- src/components/main/FilmList.jsx
+++ src/components/main/FilmList.jsx
@@ -2,6 +2,11 @@
 import { useContext } from "react";
 import GlobalContexts from "../../contexts/GlobalContexts"
 
+
+// importo le card
+import Filmcard from "./FilmCard";
+
+
 export default function FilmList() {
 
     const { films } = useContext(GlobalContexts);
@@ -10,21 +15,18 @@ export default function FilmList() {
 
 
     return (
-        <div className="container" >
-            {films.map((film) => (
-                <div className="postitem">
-                    <div key={film.id}>
-                        <h2>{film.title}</h2>
-                        <h3>{film.original_title} </h3>
-                        <span>{film.original_language}</span>
-                        <span>{film.vote_average}</span>
+        <>
+            {
+                films.map((film) => (
 
-                    </div>
-                </div>
+                    <Filmcard key={film.id} film={film} />
 
 
-            ))}
-        </div>
+                ))
+            }
+        </>
+
+
     )
 
 
