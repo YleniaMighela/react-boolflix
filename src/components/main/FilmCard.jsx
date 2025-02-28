e49f6c034ccb6c76907716148b7851881b5704f3
Ylenia <yleniamighela@gmail.com>
2025-02-28 10:25:27 +0100

    added language mismatch condition

--- src/components/main/FilmCard.jsx
+++ src/components/main/FilmCard.jsx
@@ -6,26 +6,28 @@ export default function FilmCard(props) {
 
 
 
+    // mi salvo in una costante il path del percorso della bandiera non corrispondente alla lingua
 
+    const errFlag = "../../../flags/rainbow.png"
     return (
 
 
-        <div className="container" >
-
-            <div className="postitem" key={film.id}>
-
-                <h2>Titolo {film.title}</h2>
-                <h3> Titolo originale {film.original_title} </h3>
-                {/* <span> Lingua {}</span> */}
-                <img src={`../../../flags/${film.original_language}.png`} alt="{film.original_language}" />
-                <br />
-                <span> Voto {film.vote_average}</span>
-
-            </div>
 
 
+        <div className="postitem" key={film.id}>
+
+            <h2>Titolo {film.title}</h2>
+            <h3> Titolo originale {film.original_title} </h3>
+            {/* <span> Lingua {}</span> */}
+            <img src={`../../../flags/${film.original_language}.png`} alt="{film.original_language}" onError={(e) => e.target.src = errFlag} />
+            <br />
+            <span> Voto {film.vote_average}</span>
 
         </div>
+
+
+
+
     )
 
 
